3eac6998acad88dc4208adf34e69a4fa8013b746
NishizawaShota <nishizawa@emol.jp>
2019-09-06 01:19:50 +0900

    CustomListViewで画像の表示

--- DevelopingApp/ModelTestApp/app/src/main/java/com/example/modeltestapp/ResultActivity.kt
+++ DevelopingApp/ModelTestApp/app/src/main/java/com/example/modeltestapp/ResultActivity.kt
@@ -1,22 +1,27 @@
 package com.example.modeltestapp
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_result.*
-import kotlinx.android.synthetic.main.list_item.*
 
 class ResultActivity : AppCompatActivity() {
 
     //データを用意
     val names = listOf("あじさい", "蓮", "ネモフィラ", "バラ", "ふじ")
+    val images = ArrayList<Int>()
+
+    data class PropertyData(val price: String, val imageId: Int)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,17 +31,22 @@ class ResultActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "検索結果"
 
+        //TODO: - 画面が立ち上がるときにAPI通信を始める（通信が終わるまではindicatorをだす）
+
+        //imagesをセット
+        this.names.forEach { images.add(R.drawable.test_image) }
+        Log.d("イメジ", "${this.images}")
+
+        val property = List(names.size) { i -> PropertyData(price = names[i], imageId = this.images[i])}
+
         val adapter = PropertyListAdapter(this, properties = property)
         val listView = findViewById<ListView>(R.id.result_list_view)
         listView.adapter = adapter
     }
 
 
-    data class PropertyData(val price: String)
-    val property = List(names.size) { i -> PropertyData(price = names[i])}
-
     //ViewHolderの定義
-    data class ViewHolder(val price_text_view: TextView)
+    data class ViewHolder(val priceTextView: TextView, val propertyImageView: ImageView)
 
     class PropertyListAdapter(context: Context, properties: List<PropertyData>): ArrayAdapter<PropertyData>(context, 0, properties) {
         private val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -48,15 +58,16 @@ class ResultActivity : AppCompatActivity() {
             if (view == null) {
                 view = layoutInflater.inflate(R.layout.list_item, parent, false)
                 val price_text_view = view.findViewById<TextView>(R.id.price_text_view)
-                holder = ViewHolder(price_text_view = price_text_view)
+                val property_image_view = view.findViewById<ImageView>(R.id.property_image_view)
+                holder = ViewHolder(priceTextView = price_text_view, propertyImageView = property_image_view)
                 view.tag = holder
             } else {
                 holder = view.tag as ViewHolder
             }
 
             val property = getItem(position) as PropertyData
-            holder.price_text_view.text = property.price
-
+            holder.priceTextView.text = property.price
+            holder.propertyImageView.setImageBitmap(BitmapFactory.decodeResource(context.resources, property.imageId))
             return view!!
         }
     }
