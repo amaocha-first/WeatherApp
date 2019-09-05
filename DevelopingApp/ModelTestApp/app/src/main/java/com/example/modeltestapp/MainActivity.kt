ff7c056292f3832fb7d7e3b222d85aad36c74e78
NishizawaShota <nishizawa@emol.jp>
2019-09-05 23:04:36 +0900

    画面遷移と戻る

--- DevelopingApp/ModelTestApp/app/src/main/java/com/example/modeltestapp/MainActivity.kt
+++ DevelopingApp/ModelTestApp/app/src/main/java/com/example/modeltestapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.modeltestapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupSearchBtn()
+    }
+
+    //検索ボタンを押したら次画面へ
+    private fun setupSearchBtn() {
+        val searchBtn = findViewById<Button>(R.id.button)
+        searchBtn.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- DevelopingApp/ModelTestApp/app/src/main/java/com/example/modeltestapp/ResultActivity.kt
+++ DevelopingApp/ModelTestApp/app/src/main/java/com/example/modeltestapp/ResultActivity.kt
@@ -2,11 +2,25 @@ package com.example.modeltestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        //全画面へ戻る
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "検索結果"
+    }
+
+    //前画面へ戻る
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
     }
 }
